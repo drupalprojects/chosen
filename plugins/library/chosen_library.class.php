75c7bc60fd551a58a418c182c4a55c8047038e0c
Hydraner <drupal@pascalcrott.de>
2013-08-30 11:34:20 +0200

    Make configuration plugable

--- plugins/library/chosen_library.class.php
+++ plugins/library/chosen_library.class.php
@@ -5,42 +5,109 @@
  */
 class chosen_library implements chosen_library_interface {
 
+  public $title = 'Chosen';
+  public $website = 'http://harvesthq.github.com/chosen';
+  public $version = '0.10.0';
   public $directory = 'chosen';
+  public $library_name = 'chosen';
   public $file_name = 'chosen.jquery.min.js';
+  public $css = 'chosen.css';
 
 
-  public function library() {
-    $module_path = drupal_get_path('module', 'chosen');
+  public function options($default_values) {
+    $form = array();
+    // dsm($default_values);
 
-    return array(
-      'title' => 'Drupal Chosen integration',
-      'website' => 'http://drupal.org/project/chosen',
-      'version' => '1.0',
-      'js' => array(
-        $module_path . '/chosen.js' => array(),
-        array(
-          'data' => array(
-            'chosen' => array(
-              'selector' => variable_get('chosen_jquery_selector', 'select:visible'),
-              'minimum' => variable_get('chosen_minimum', 20),
-              'minimum_width' => variable_get('chosen_minimum_width', 200),
-              'search_contains' => (variable_get('chosen_search_contains', FALSE)) ? TRUE : FALSE,
-              'disable_search' => (variable_get('chosen_disable_search', FALSE)) ? TRUE : FALSE,
-              'disable_search_threshold' => variable_get('chosen_disable_search_threshold', 0),
-              'placeholder_text_multiple' => t(variable_get('chosen_placeholder_text_multiple', 'Choose some options')),
-              'placeholder_text_single' => t(variable_get('chosen_placeholder_text_single', 'Choose an option')),
-              'no_results_text' => t(variable_get('chosen_no_results_text', 'No results match')),
-            ),
-          ),
-          'type' => 'setting',
-        ),
-      ),
-      'css' => array(
-        $module_path . '/css/chosen-drupal.css' => array(),
-      ),
-      'dependencies' => array(
-        array('chosen', 'chosen'),
-      ),
-    );
+
+  $form['minimum_single'] = array(
+    '#type' => 'select',
+    '#title' => t('Minimum number of options for single select'),
+    '#options' => drupal_map_assoc(array_merge(array('0' => t('Always Apply')), range(1, 25))),
+    '#default_value' => isset($default_values['minimum_single']) ? $default_values['minimum_single'] : 20,
+    '#description' => t('The minimum number of options to apply Chosen for single select fields. Example : choosing 10 will only apply Chosen if the number of options is greater or equal to 10.')
+  );
+
+  $form['minimum_multiple'] = array(
+    '#type' => 'select',
+    '#title' => t('Minimum number of options for multi select'),
+    '#options' => drupal_map_assoc(array_merge(array('0' => t('Always Apply')), range(1, 25))),
+    '#default_value' => isset($default_values['minimum_multiple']) ? $default_values['minimum_multiple'] : 20,
+    '#description' => t('The minimum number of options to apply Chosen for multi select fields. Example : choosing 10 will only apply Chosen if the number of options is greater or equal to 10.')
+  );
+
+  $form['disable_search_threshold'] = array(
+    '#type' => 'select',
+    '#title' => t('Minimum number to show Search on Single Select'),
+    '#options' => drupal_map_assoc(array_merge(array('0' => 'Never Apply'), range(1, 25))),
+    '#default_value' => isset($default_values['disable_search_threshold']) ? $default_values['disable_search_threshold'] : 0,
+    '#description' => t('The minimum number of options to apply Chosen search box. Example : choosing 10 will only apply Chosen search if the number of options is greater or equal to 10.')
+  );
+
+  $form['minimum_width'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Minimum width of widget'),
+    '#field_suffix' => 'px',
+    '#required' => TRUE,
+    '#size' => 3,
+    '#default_value' => isset($default_values['minimum_width']) ? $default_values['minimum_width'] : 200,
+    '#description' => t('The minimum width of the Chosen widget.'),
+  );
+
+  $form['jquery_selector'] = array(
+    '#type' => 'textarea',
+    '#title' => t('Apply Chosen to the following elements'),
+    '#description' => t('A comma-separated list of jQuery selectors to apply Chosen to, such as <code>select#edit-operation, select#edit-type</code> or <code>.chosen-select</code>. Defaults to <code>select</code> to apply Chosen to all <code>&lt;select&gt;</code> elements.'),
+    '#default_value' => isset($default_values['jquery_selector']) ? $default_values['jquery_selector'] : 'select:visible',
+  );
+
+  $form['options'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Chosen options'),
+  );
+  $form['options']['search_contains'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Search also in the middle of words'),
+    '#default_value' => isset($default_values['search_contains']) ? $default_values['search_contains'] : FALSE,
+    '#description' => t('Per default chosen searches only at beginning of words. Enable this option will also find results in the middle of words.
+      Example: Search for <em>land</em> will also find <code>Switzer<strong>land</strong></code>.'),
+  );
+  $form['options']['disable_search'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Disable search box'),
+    '#default_value' => isset($default_values['disable_search']) ? $default_values['disable_search'] : FALSE,
+    '#description' => t('Enable or disable the search box in the results list to filter out possible options.'),
+  );
+  $form['options']['use_theme'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Use the default chosen theme'),
+    '#default_value' => isset($default_values['use_theme']) ? $default_values['use_theme'] : TRUE,
+    '#description' => t('Enable or disable the default chosen CSS file. Disable this option if your theme contains custom styles for Chosen replacements.'),
+  );
+
+  $form['strings'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Chosen strings'),
+  );
+  $form['strings']['placeholder_text_multiple'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Placeholder text of multiple selects'),
+    '#required' => isset($default_values['placeholder_text_multiple']) ? $default_values['placeholder_text_multiple'] : TRUE,
+    '#default_value' => 'Choose some options',
+  );
+  $form['strings']['placeholder_text_single'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Placeholder text of single selects'),
+    '#required' => isset($default_values['placeholder_text_single']) ? $default_values['placeholder_text_single'] : TRUE,
+    '#default_value' => 'Choose an option',
+  );
+  $form['strings']['no_results_text'] = array(
+    '#type' => 'textfield',
+    '#title' => t('No results text'),
+    '#required' => isset($default_values['no_results_text']) ? $default_values['no_results_text'] : TRUE,
+    '#default_value' => 'No results match',
+  );
+
+    return $form;
   }
+
 }
---
+++ plugins/library/select2_library.class.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * TODO: Documentation.
+ */
+class select2_library implements chosen_library_interface {
+
+  public $title = 'Select2';
+  public $website = 'http://ivaynberg.github.io/select2';
+  public $version = '3.42.0';
+  public $directory = 'select2';
+  public $library_name = 'select2';
+  public $file_name = 'select2.jquery.min.js';
+  public $css = 'select2.css';
+
+
+  public function options($default_values) {
+    $form = array();
+
+
+
+    $form['minimum_single'] = array(
+      '#type' => 'select',
+      '#title' => t('Minimum number of options for single select'),
+      '#options' => drupal_map_assoc(array_merge(array('0' => t('Always Apply')), range(1, 25))),
+      '#default_value' => isset($default_values['minimum_single']) ? $default_values['minimum_single'] : 20,
+      '#description' => t('The minimum number of options to apply Chosen for single select fields. Example : choosing 10 will only apply Chosen if the number of options is greater or equal to 10.')
+    );
+
+    return $form;
+  }
+
+}
